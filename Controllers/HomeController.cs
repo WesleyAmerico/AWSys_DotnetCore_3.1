ea66f939ffcf16129a775801c59bdf286aff1c1b
WesleyAmerico <wesley.americo@live.com>
2022-12-20 15:47:59 -0300

    Criando resopistorio no github

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using AWSys_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSys_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async void ExecutarFuncaoPrincipal()
+        {
+           
+        }
+        [HttpPost]
+        public async void GetValoresBalanca()
+        {
+
+        }
+        [HttpPost]
+        public async void GetStatusBalanca()
+        {
+
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
